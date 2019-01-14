c80dc11554104797db986134faa2d9eb8b821956
20170916 <1334320028@qq.coim>
2019-01-15 00:06:07 +0800

    simple unit test in python

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # author: lo time:2019-01-13
+import unittest
 
 
-def bubbleSort(arr):
-    for i in range(len(arr)-1):
-        for j in range(len(arr)-i-1):
-            if arr[j]>arr[j+1]:
-                arr[j],arr[j+1]=arr[j+1],arr[j]
-    return arr
+class TestSort(unittest.TestCase):
+    def tearDown(self):
+        # 每个测试用例执行之后做操作
+        print('after')
+
+    def setUp(self):
+        # 每个测试用例执行之前做操作
+        print('before')
+
+    @staticmethod
+    def bubbleSort(arr):
+        for i in range(len(arr)-1):
+            for j in range(len(arr)-i-1):
+                if arr[j]>arr[j+1]:
+                    arr[j],arr[j+1]=arr[j+1],arr[j]
+        return arr
+
+    def test_bubble(self):
+        arr=[3,2,6,9,5,1,4,7,8,0]
+        print TestSort.bubbleSort(arr)
 
 
-arr=[3,2,6,9,5,1,4,7,8,0]
-print bubbleSort(arr)
+if __name__ == '__main__':
+    unittest.main()
+
