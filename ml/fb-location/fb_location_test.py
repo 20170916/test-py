a4d5e3d2bb26cef266db9647b8eabdbbfea411f6
20170916 <1334320028@qq.com>
2019-02-20 23:56:32 +0800

    GridSearchCV

--- ml/fb-location/fb_location_test.py
+++ ml/fb-location/fb_location_test.py
@@ -5,7 +5,7 @@ import unittest
 from datetime import date
 
 from sklearn.datasets import load_iris, fetch_20newsgroups, load_boston
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
@@ -75,5 +75,82 @@ class FBLocationTest(unittest.TestCase):
 
         return None
 
-    def test_variance(self):
+    def test_FBLocation_KNeighbors(self):
         FBLocationTest.knncls()
+
+    @staticmethod
+    def knnclsGridSearchCV():
+        """
+        K近邻预测用户签到位置
+        :return:None
+        """
+        # 读取数据
+        data = pd.read_csv("./data/train.csv")
+        print(data.head(10))
+        # 处理数据
+        # 1 缩小数据，对查询数据进行筛选
+        data = data.query("x > 1.0 & x < 1.25 &y > 2.5 & y < 2.75")
+
+        # 处理时间数据
+        time_value = pd.to_datetime(data['time'], unit='s')
+
+        print(time_value)
+
+        # 把日期数据格式转换成字典格式
+        time_value = pd.DatetimeIndex(time_value)
+
+        # 构造时间特征
+        data["day"] = time_value.day
+        data["hour"] = time_value.hour
+        data["weekday"] = time_value.weekday
+
+        # 把时间戳特征删除,pandas中 axis=1表示删除列，sklearn中axis=0表示删除列
+        data = data.drop(["time"], axis=1)
+        print(data)
+
+        # 删除签到数量少于n的数据
+        place_count = data.groupby("place_id").count()
+        tf = place_count[place_count.row_id > 3].reset_index()
+        data = data[data["place_id"].isin(tf.place_id)]
+
+        print(data)
+
+        # 取出数据中的特征值和目标
+        y = data["place_id"]
+        x = data.drop(["place_id"], axis=1)
+
+        # 分割训练集和测试集
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+
+        # 特征工程（标准化）
+        std = StandardScaler()
+        # 对测试集和训练集的特征值进行标准化
+        x_train = std.fit_transform(x_train)
+        x_test = std.transform(x_test)
+
+        # 算法流程
+        knn = KNeighborsClassifier()
+        # # fit predict score
+        # knn.fit(x_train, y_train)
+        # # 得出预测结果
+        # y_predict = knn.predict(x_test)
+        # print("预测的目标签到位置为：", y_predict)
+        # # 输出预测准确率
+        # print("预测准确率：", knn.score(x_test, y_test))
+
+        # 构造一些参数的值进行搜索
+        param = {"n_neighbors": [3, 5, 10]}
+
+        # 进行网格搜索
+        gc = GridSearchCV(knn, param_grid=param, cv=2)
+        gc.fit(x_train, y_train)
+
+        # 预测准确率
+        print("在测试集上准确率：", gc.score(x_test, y_test))
+        print("在交叉验证中最好的结果：", gc.best_score_)
+        print("最好的模型：", gc.best_estimator_)
+        print("每个超参数每次交叉验证的结果：", gc.cv_results_)
+        return None
+
+    def test_FBLocation_KNeighbors_knnclsGridSearchCV(self):
+        FBLocationTest.knnclsGridSearchCV()
