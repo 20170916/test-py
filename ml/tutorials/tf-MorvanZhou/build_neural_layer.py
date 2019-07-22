3b5d211bee6f20d4060895e220f400680fe41bc5
20170916 <1334320028@qq.com>
2019-07-23 00:28:52 +0800

    plot result

--- ml/tutorials/tf-MorvanZhou/build_neural_layer.py
+++ ml/tutorials/tf-MorvanZhou/build_neural_layer.py
@@ -3,6 +3,7 @@
 # author: lo time:2019-07-21
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
@@ -59,10 +60,39 @@ def build_neural_layer():
     session = tf.Session()
     session.run(init)
 
+    # 生成一个图片匡
+    fig = plt.figure()
+    # 连续画图,编号1，1，1
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter(x_data, y_data)
+    # plt.show 会暂停整个程序，ion可以连续的plt
+    # 老版本是在show方法指定Block=false
+    plt.ion()
+    plt.show()
+
     for i in range(1000):
         session.run(train_step, feed_dict={xs: x_data, ys: y_data})
         if i % 50 == 0:
             print(session.run(loss, feed_dict={xs: x_data, ys: y_data}))
+
+            try:
+                # catch掉第一次抹除时没有线
+                ax.lines.remove(lines[0])
+                pass
+            except Exception:
+                pass
+
+            # 每50步显示一次预测数据
+            prediction_value = session.run(prediction, feed_dict={xs: x_data})
+            # 将prediction的值以曲线的形式plot上去
+            # 红色的线：r-
+            # 宽度为5：lw=5
+
+            lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+            # 抹除上次画的线
+            # ax.lines.remove(lines[0])
+            # 暂停0.1秒后继续
+            plt.pause(0.1)
 
     return None
 
