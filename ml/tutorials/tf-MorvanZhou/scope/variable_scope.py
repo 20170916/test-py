a4ff4e1f0c146308874576a8ef2f12eb30c6f571
20170916 <1334320028@qq.com>
2019-08-07 22:59:54 +0800

    scope

---
+++ ml/tutorials/tf-MorvanZhou/scope/variable_scope.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author: lo time:2019-08-07
+import tensorflow as tf
+
+with tf.variable_scope("a_variable_scope") as scope:
+    initializer = tf.constant_initializer(value=3)
+    var3 = tf.get_variable(name='var3', shape=[1], dtype=tf.float32, initializer=initializer)
+    scope.reuse_variables()
+    var3_reuse = tf.get_variable(name='var3', )
+    var4 = tf.Variable(name='var4', initial_value=[4], dtype=tf.float32)
+    var4_reuse = tf.Variable(name='var4', initial_value=[4], dtype=tf.float32)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print(var3.name)  # a_variable_scope/var3:0
+    print(sess.run(var3))  # [ 3.]
+    print(var3_reuse.name)  # a_variable_scope/var3:0
+    print(sess.run(var3_reuse))  # [ 3.]
+    print(var4.name)  # a_variable_scope/var4:0
+    print(sess.run(var4))  # [ 4.]
+    print(var4_reuse.name)  # a_variable_scope/var4_1:0
+    print(sess.run(var4_reuse))  # [ 4.]
+
+if __name__ == "__main__":
+    print("ok")
